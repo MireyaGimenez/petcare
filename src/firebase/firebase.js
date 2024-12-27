a337bde9b5ef8f52544041aa946ddd417cd9797e
MireyaGimenez <reyagimenezz@uoc.edu>
2024-12-27 22:31:37 +0100

    fix: hide api key and other tokens from github

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhfcEmzH6CTJWAz3P9q4phVSl40mCaKC4",
-  authDomain: "petcare-a3a6a.firebaseapp.com",
-  projectId: "petcare-a3a6a",
-  storageBucket: "petcare-a3a6a.firebasestorage.app",
-  messagingSenderId: "1083901992278",
-  appId: "1:1083901992278:web:a075b5149bae513e72caf7",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
